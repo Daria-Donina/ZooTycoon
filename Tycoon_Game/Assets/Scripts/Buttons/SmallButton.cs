70b760fc4c31260db6e5e8ad1bc622c7996f9684
Daria-Donina <donina.daria@yandex.ru>
2020-05-03 02:29:30 +0300

    started maling a constructor

---
+++ Tycoon_Game/Assets/Scripts/Buttons/SmallButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Buttons.Classes;
+using UnityEngine.Tilemaps;
+
+public class SmallButton : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap pen;
+
+    [SerializeField]
+    private Tile fence;
+
+    [SerializeField]
+    private GameObject bear;
+
+    public void OnClick()
+    {
+        CreateTileAtPoint(bear.transform.position);
+    }
+
+    void CreateTileAtPoint(Vector3 pos)
+    {
+        pen.SetTile(pen.WorldToCell(pos), fence);
+    }
+}
