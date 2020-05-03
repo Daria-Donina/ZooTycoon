0ff41ab423713a114e35864f2c20052495754466
NadiaSob <sobo_na@mail.ru>
2020-05-03 20:37:28 +0300

    Added needs bars on the AnimalNeedsPanel

--- Tycoon_Game/Assets/Scripts/Animals/AnimalNeeds.cs
+++ Tycoon_Game/Assets/Scripts/Animals/AnimalNeeds.cs
@@ -4,9 +4,31 @@ using UnityEngine.UIElements;
 public class AnimalNeeds : MonoBehaviour
 {
     [SerializeField]
+    private Bar foodBarPrefab;
+
+    [SerializeField]
+    private Bar waterBarPrefab;
+
+    [SerializeField]
+    private Bar entertainmentBarPrefab;
+
+    [SerializeField]
+    private Bar inhabitancyBarPrefab;
+
+    [SerializeField]
+    private Bar welfareBarPrefab;
+
+    private Bar foodBar;
+    private Bar waterBar;
+    private Bar entertainmentBar;
+    private Bar inhabitancyBar;
+    private Bar welfareBar;
+
+    [SerializeField]
     private GameObject animalNeedsPanelPrefab;
 
     private GameObject animalNeedsPanel;
+
 
     [SerializeField]
     private float welfare;
@@ -31,35 +53,66 @@ public class AnimalNeeds : MonoBehaviour
         entertainment = 100f;
         inhabitancy = 0;
 
+        CalculateInhabitancy();
+
         animalNeedsPanel = Instantiate(animalNeedsPanelPrefab, transform.position, transform.rotation) as GameObject;
         animalNeedsPanel.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+
+        var position = transform.position;
+        position.x = -282;
+        position.y = 30;
+        foodBar = Instantiate(foodBarPrefab, position, transform.rotation) as Bar;
+        foodBar.transform.SetParent(animalNeedsPanel.transform, false);
+
+        position.x = -67;
+        inhabitancyBar = Instantiate(inhabitancyBarPrefab, position, transform.rotation) as Bar;
+        inhabitancyBar.transform.SetParent(animalNeedsPanel.transform, false);
+
+        position.y = -35;
+        entertainmentBar = Instantiate(entertainmentBarPrefab, position, transform.rotation) as Bar;
+        entertainmentBar.transform.SetParent(animalNeedsPanel.transform, false);
+
+        position.x = -282;
+        waterBar = Instantiate(waterBarPrefab, position, transform.rotation) as Bar;
+        waterBar.transform.SetParent(animalNeedsPanel.transform, false);
+
+        position.x = 151;
+        position.y = -3;
+        welfareBar = Instantiate(welfareBarPrefab, position, transform.rotation) as Bar;
+        welfareBar.transform.SetParent(animalNeedsPanel.transform, false);
+
         animalNeedsPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        
-    }*/
-
+    [System.Obsolete]
     private void FixedUpdate()
     {
         if (food > 0)
         {
-            food -= 0.003f;
+            food -= 0.3f;
         }
 
         if (water > 0)
         {
-            water -= 0.003f;
+            water -= 0.3f;
         }
 
         if (entertainment > 0)
         {
-            entertainment -= 0.003f;
+            entertainment -= 0.3f;
         }
 
+        CalculateInhabitancy();
         CalculateWelfare();
+
+        if (animalNeedsPanel.active)
+        {
+            foodBar.SetValue((int)food);
+            inhabitancyBar.SetValue((int)inhabitancy);
+            entertainmentBar.SetValue((int)entertainment);
+            waterBar.SetValue((int)water);
+            welfareBar.SetValue((int)welfare);
+        }
     }
 
     private void OnMouseDown()
@@ -71,12 +124,17 @@ public class AnimalNeeds : MonoBehaviour
             panel.SetActive(false);
         }
 
+        foodBar.SetValue((int)food);
+        inhabitancyBar.SetValue((int)inhabitancy);
+        entertainmentBar.SetValue((int)entertainment);
+        waterBar.SetValue((int)water);
+        welfareBar.SetValue((int)welfare);
+
         animalNeedsPanel.SetActive(true);
     }
 
     public void CalculateInhabitancy()
     {
-        
     }
 
     /// <summary>
