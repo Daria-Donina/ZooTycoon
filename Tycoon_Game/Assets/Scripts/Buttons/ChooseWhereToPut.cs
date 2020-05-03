4e9a100d46574a5f3686572f77197860bf08bfec
Daria-Donina <donina.daria@yandex.ru>
2020-05-03 20:33:53 +0300

    added fences adding

---
+++ Tycoon_Game/Assets/Scripts/Buttons/ChooseWhereToPut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ChooseWhereToPut : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectingPanel;
+
+    [SerializeField]
+    private GameObject sizePanel;
+
+    [SerializeField]
+    private Tilemap tilemap;
+
+    [SerializeField]
+    private Tile fence;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var vector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            vector.z = 0;
+            tilemap.SetTile(tilemap.WorldToCell(vector), fence);
+
+            selectingPanel.SetActive(false);
+            sizePanel.SetActive(true);
+        }
+    }
+}
---
+++ Tycoon_Game/Assets/Scripts/Buttons/OpenCloseButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject panelToOpen;
+
+    [SerializeField]
+    private GameObject panelToClose;
+
+    public void OnClick()
+    {
+        panelToOpen.gameObject.SetActive(true);
+        panelToClose.gameObject.SetActive(false);
+    }
+}
